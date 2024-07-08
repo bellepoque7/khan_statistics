dc2e9bb460997a679c40a7b1bc0c06e5a2360822
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2024-07-08 20:14:47 +0900

    Create 3단원.R

---
+++ 3단원.R
@@ -0,0 +1,6 @@
+#모집단의 표준편차
+get_av_var <- function(list) {
+  my_mean <- sum(list) / length(list)
+  my_var <- sum((list - my_mean)^2) / (length(list) - 1)
+  return(c(round(my_mean, 2), round(my_var, 2)))
+}
